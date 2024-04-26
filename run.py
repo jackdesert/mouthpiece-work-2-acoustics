29c565ce53f28b9e5732892d31324269f85f88a0
jack <jack>
2024-04-25 20:11:37 -0500

    Build index;

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import re
 import json
 from pathlib import Path
 import quopri
+import shutil
 import ipdb
 
 # Path to your .warc file
@@ -13,15 +14,26 @@ NON_SLUG_CHARS = re.compile(r'[^a-z0-9]')
 SPACE = ' '
 HYPHEN = '-'
 
-def build_filename(*, parent_id, this_id):
+# Delete and recreate html directory
+# so it's fresh each time
+shutil.rmtree(str(HTML_DIR), ignore_errors=True)
+HTML_DIR.mkdir(parents=True, exist_ok=True)
+
+# There are about 12_000 posts
+ZFILL = 5
+
+# Only set this to small for fast iteration
+MAX = 500_000
+
+def build_filename(parent_id):
     """
     Where to write the data
     """
     subject_slug = subject_mapping.get(parent_id, 'unknown')
-    parent_slug = f'{parent_id}__{subject_slug}'
-    dir_ = HTML_DIR / parent_slug
-    dir_.mkdir(parents=True, exist_ok=True)
-    return dir_ / f'{this_id}.html'
+    parent_id_filled = str(parent_id).zfill(ZFILL)
+    # Store as a text file so it shows line feeds
+    parent_slug = f'{parent_id_filled}__{subject_slug}.txt'
+    return HTML_DIR / parent_slug
 
 def append_to_file(*, content:str, fname:Path):
     with fname.open('a') as appender:
@@ -42,7 +54,11 @@ types = set()
 # Open the .warc file
 with open(warc_file_path, 'rb') as warc_file:
     # Iterate over the records in the .warc file
+    count = 0
     for record in ArchiveIterator(warc_file):
+        count += 1
+        if count > MAX:
+            break
         # Get the URL of the response
         # Headers of interest include:
         headers = record.rec_headers
@@ -78,8 +94,10 @@ with open(warc_file_path, 'rb') as warc_file:
             this_id = content['messageId']
             parent_id = content['topicFirstRecord']
             subject = content['subject']
-            subject_mapping[this_id] = slugify(subject)
-            fname = build_filename(parent_id=parent_id, this_id=this_id)
+
+            # Only need parents in mapping
+            subject_mapping[parent_id] = slugify(subject)
+            fname = build_filename(parent_id)
             append_to_file(fname=fname, content=f'\n\n===========================================\n{content}')
             #topicNextRecord = content['topicNextRecord']
             #topicPrevRecord = content['topicPrevRecord']
@@ -92,4 +110,12 @@ with open(warc_file_path, 'rb') as warc_file:
         #print("URL:", url)
         #print("Content:", content)
 
-print(types)
+
+lines = ['<h1>Hello</h1>', '<ol>']
+for parent_id_, subject_ in subject_mapping.items():
+    fname_ = build_filename(parent_id_)
+    lines.append(f'<li><a href="../{fname_}" target="_blank">{subject_}</a></li>')
+lines.append('</ol>')
+
+with open('html/index.html', 'w') as writer:
+    writer.write('\n'.join(lines))
